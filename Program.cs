2c4814c7a545d68e3457f2106abde304f024ccdb
Pablo Zaidenvoren <pabloz@lagash.com>
2018-10-26 11:33:00 -0300

    initial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var jsonStr = File.ReadAllText("data.json");
+            var json = JObject.Parse(jsonStr);
+
+            foreach (JObject q in json["questions"].AsJEnumerable())
+            {
+                var question = q.Value<string>("q");
+                System.Console.WriteLine($"<p class='question'>{question}</p>");
+                var options = q["a"].AsJEnumerable();
+                System.Console.WriteLine("<ul>");
+                foreach (JObject o in options)
+                {
+                    var text = o.Value<string>("option");
+                    var correct = o.Value<bool>("correct");
+                    var cssClass = correct ? "correct" : "incorrect";
+                    System.Console.WriteLine($"<li class='{cssClass}'>{text}</li>");
+                }
+                System.Console.WriteLine("</ul>");
+            }
+            System.Console.WriteLine("<br />");
+            System.Console.WriteLine("<br />");
+        }
+    }
+}
